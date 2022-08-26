8f3c03284d9d3c5046eaf17a6b488f0c07aa5b55
JUAN-BH <juanmita_bh@hotmail.com>
2022-08-26 16:23:54 -0500

    intentado arreglar env

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -3,11 +3,12 @@ const API = process.env.API;
 const getData = async (id) => {
   const apiURl = id ? `${API}${id}` : API;
   try {
-    const response = await fetch(apiURl);
-    const data = await response.json();
+    var response = await fetch(apiURl);
+    var data = await response.json();
+    console.log(data);
     return data.results[0];
   } catch (error) {
-    console.log("Fetch Error", error);
+    console.log("Fetch Error", error, response, response.body);
   }
 };
 
